0e6515e71d2b9ef9964e561c7db711ce78f238a5
Nick Edwin <nickedwin85@gmail.com>
2020-07-01 13:44:55 -0600

    Created new shelter link

---
+++ spec/features/shelters/user_can_see_shelters_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "shelters index page", type: :feature do
+  it "can see shelters index" do
+    shelter_1 = Shelter.create(name: "Pawsitively Pawsome Animal Shelter")
+    shelter_2 = Shelter.create(name: "Raise the Ruff Animal Shelter")
+
+    visit "/shelters"
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_2.name)
+  end
+end
