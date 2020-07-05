9abd2878c44f28693120cb3a338ddd973a63a3b6
Nick Edwin <nickedwin85@gmail.com>
2020-07-05 16:42:39 -0600

    Updated shelter index page tests

--- spec/features/shelters/index_page_spec.rb
+++ spec/features/shelters/index_page_spec.rb
@@ -9,13 +9,30 @@ RSpec.describe "shelters index page", type: :feature do
 
     expect(page).to have_content(shelter_1.name)
     expect(page).to have_content(shelter_2.name)
-
+    expect(page).to have_link("#{shelter_1.name}", :href=>"/shelters/#{shelter_1.id}")
+    expect(page).to have_link("#{shelter_1.name}", :href=>"/shelters/#{shelter_1.id}")
   end
 
-  it "contains a New Shelter Button" do
+  it "Link to edit shelter" do
+    shelter_1 = Shelter.create(name: "Pawsitively Pawsome Animal Shelter")
+
+    visit "/shelters"
+
+    expect(page).to have_link("Edit #{shelter_1.name}'s Information", :href=>"/shelters/#{shelter_1.id}/edit")
+  end
+
+  it "contains a New Shelter Link" do
 
     visit "/shelters"
 
     expect(page).to have_link("New Shelter", :href=>"/shelters/new")
+  end
+
+  it "contains a Delete Button" do
+    shelter_1 = Shelter.create(name: "Pawsitively Pawsome Animal Shelter")
+
+    visit "/shelters"
+
+    expect(page).to have_button("Delete #{shelter_1.name}")
   end
 end
